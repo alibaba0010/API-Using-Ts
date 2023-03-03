c044fc230f13732e59f9f8f09ec344fcfaf1e168
alibaba0010 <zakariyahali100@gmail.com>
2023-03-03 20:26:59 +0100

    Updating app functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,46 +1,46 @@
 import express, { Application } from "express";
-// import cors from "cors";
-// import compression from "compression";
-// import morgan from "morgan";
-// import helmet from "helmet";
-// import connectDB from "./db";
+import cors from "cors";
+import compression from "compression";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./db";
 
 // import Error from "@/middleware/error.middleware";
 // import Controller from "@/controllers/controller.interface";
 
-// class App {
-//   public express: Application;
-//   public port: number;
-//   public uri = process.env.MONGO_URL;
+class App {
+  public express: Application;
+  public port: number;
+  public uri = process.env.MONGO_URL;
 
-//   constructor(controllers: Controller[], port: number, uri :string) {
-//     this.express = express();
-//     this.port = port;
+  constructor(controllers: Controller[], port: number, uri :string) {
+    this.express = express();
+    this.port = port;
 
-//     this.initialiseDatabaseConnection();
-//     this.initialiseMiddleware();
-//     this.initialiseControllers(controllers);
-//     this.initialiseErrorHandling();
-//   }
-//   private initialiseMiddleware(): void {
-//     this.express.use(helmet());
-//     this.express.use(cors());
-//     this.express.use(morgan("dev"));
-//     this.express.use(express.json());
-//     this.express.use(express.urlencoded({ extended: false }));
-//     this.express.use(compression());
-//   }
-//   private initialiseControllers(controllers: Controller[]): void {
-//     controllers.forEach((controller: Controller) => {
-//       this.express.use("/api/v1", controller.router);
-//     });
-//   }
-//   private initialiseErrorHandling(): void {
-//     this.express.use(Error);
-//   }
-//   private initialiseDatabaseConnection(): void {
-//     connectDB(uri);
-//   }
-// }
+    this.initialiseDatabaseConnection();
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+    this.initialiseErrorHandling();
+  }
+  private initialiseMiddleware(): void {
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(morgan("dev"));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+  private initialiseControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use("/api/v1", controller.router);
+    });
+  }
+  private initialiseErrorHandling(): void {
+    this.express.use(Error);
+  }
+  private initialiseDatabaseConnection(): void {
+    connectDB(uri);
+  }
+}
 
 export default App;
